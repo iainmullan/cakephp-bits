e718751b63cf3bf7a38e35e24c69f0207d4d2cf3
ebotunes <iain.mullan@gmail.com>
2009-07-16 01:41:56 +0100

    added class documentation

--- helpers/sidebar.php
+++ helpers/sidebar.php
@@ -1,6 +1,37 @@
 <?
 /**
  *
+ * The Sidebar Helper allows you to specify blocks of content from your view files, and then render them all at once from
+ * your layout. Content blocks can be rendered as Cake elements, or 'inline'.
+ *
+ * View code:
+ *
+ *	$sidebar->addBox(array('title' => 'Sidebox One', 'element'=>'sidebox_1'));
+ *
+ *	$sidebar->addBox(array('title' => 'Sidebox B', 'element'=>'sidebox_b'));
+ *
+ *	$sidebar->startBox(array('title' => 'Inline box', 'element'=>'my_inline_box'));
+ *		<p> Click <a href="/">here</a> to go to the homepage </p>
+ *	$sidebar->endBox();
+ *
+ * HTML Rendered:
+ *
+ *	<div id="sidebar">
+ * 		<div id="sidebox_1" class="sidebox">
+ * 			<h4>Sidebox One</h4>
+ * 			... contents of views/elements/sidebox_1.ctp ...
+ *		</div>
+ * 		<div id="sidebox_1" class="sidebox">
+ * 			<h4>Sidebox B</h4>
+ * 			... contents of views/elements/sidebox_b.ctp ...
+ *		</div>
+ * 		<div id="my_inline_box" class="sidebox">
+ * 			<h4>Inline Box</h4>
+ * 			<p> Click <a href="/">here</a> to go to the homepage </p>
+ *		</div>
+ *	</div>
+ *
+ *
  * @created May 2009
  * @author Iain Mullan , www.ebotunes.com
  *
@@ -50,48 +81,20 @@ class SidebarHelper extends Helper {
 	}
 
 	/**
-	 * @see addBox
-	 * @deprecated Use addBox and supply all params as a single indexed array
-	 */
-	function add($title, $element, $params, $index = false) {
-		$this->addElement($title, $element, $params, $index);
-    }
-
-	/**
-	 * @see addBox
-	 * @deprecated Use addBox and supply all params as a single indexed array
-	 */
-	function addElement($title, $element, $params, $index = false) {
-		$box = array('title' => $title, 'element' => $element, 'params' => $params);
-		$this->addBox($box, $index);
-    }
-
-	/**
-	 * @see addBox
-	 * @deprecated Use addBox and supply all params as a single indexed array
-	 */
-	function addContent($title, $content, $index = false) {
-		$box = array('title' => $title, 'content' => $content, 'element' => '', 'index' => $index);
-		$this->addBox($box, $index);
-    }
-
-	/**
+	 *	Takes a single parameter, an indexed array with the following keys:
 	 *
-	 * 	- title - The title of the box, which will be wrapped in an HTML tag specified by $options['title_tag']. A value of null will prevent the title tag being rendered at all.
 	 * 	- content - The HTML content of the box.
 	 * 	- element - If 'content' is empty, this will be interpreted as a Cake element (defined in views/elements/<element>.ctp). If 'content' is non-empty, this will be used as the ID of this box's div
+	 * 	- title - The title of the box, which will be wrapped in an HTML tag specified by $options['title_tag']. A value of null will prevent the title tag being rendered at all.
 	 *  - params - A parameter array to be passed to renderElement
 	 *  - index - an optional numeric index, specifying the position of this box in the sidebar, starting at 0
 	 *
+	 * The passed box array must contain *either* a (non-empty) content or element. All other keys are optional.
+	 *
 	 * @param box
 	 */
-    function addBox($box, $index = false) {
+    function addBox($box) {
 		$this->enable();
-
-		// backwards compatibility for $index parameter
-		if (!isset($box['index'])) {
-			$box['index'] = $index;
-		}
 
 		$box = array_merge($this->_defaultBox, $box);
 
@@ -118,7 +121,17 @@ class SidebarHelper extends Helper {
 	 * element or content options supplied, and instead render the content of the box 'inline' in your view.
 	 * End the content buffer with $sidebar->endBox();
 	 *
-	 * @return true if a sidebox buffer is started succesfully, false otherwise (e.g. in case youve already called
+	 * Note that the 'element' index may (should!) still be specified, in order to give the box's div an ID. Also
+	 * note that if 'content' is specified, it will be ultimately overwritten by the inline content once endBox() is called.
+	 *
+	 * Example Usage (in your view file):
+	 *
+	 * 		$sidebar->startBox(array('title'=>'My inline sidebox', 'element'=>'my_inline_box'));
+	 *  		<p>This is some content I just want to throw into a <b>sidebox</b> without having to create a <b>.ctp</b> file in <code>views/elements</code>.</p>
+	 * 		$sidebar->endBox();
+	 *
+	 *
+	 * @return true if a sidebox buffer is started succesfully, false otherwise (i.e. in case youve already called
 	 * startBox without a corresponding endBox)
 	 */
     function startBox($box) {
